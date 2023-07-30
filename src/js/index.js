f5a2bd8cc9114cac4db80afda45e3f49d9f2838e
uiuxkimabe <uiuxkimabe@gmail.com>
2023-07-31 00:23:29 +0700

    add items to detail cart

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,6 @@
 const minBtn = document.querySelectorAll(".minQty");
 const qtyValue = document.querySelectorAll(".qtyValue");
 const addBtn = document.querySelectorAll(".addQty");
-const addToCart = document.querySelectorAll(".addToCart");
 
 // Menambahkan & Mengurangi Qty Pesanan
 function qty(params) {
@@ -36,17 +35,6 @@ function qty(params) {
       alert(`Jumlah Pesanan Anda Masih ${count}`);
     }
   });
-  addToCart.forEach((element) => {
-    element.addEventListener("click", () => {
-      let result = count + 1;
-      count++;
-      const qtyInCart = document.querySelector(".cart .qtyInCart");
-      if ((result) => 1) {
-        qtyInCart.innerHTML = result;
-        qtyInCart.style.display = "block";
-      }
-    });
-  });
 }
 
 // card eksekusi function
@@ -67,7 +55,7 @@ const filterItems = document.querySelectorAll("main .menu");
 // destructuring filter button
 const [all, appetizer, mainCourse, dessert] = filterBtn;
 
-// Filter Menu
+// Filter Menu Function
 filterItems.forEach((element) => {
   all.addEventListener("click", () => {
     element.style.display = element.classList.contains("all") ? "flex" : "none";
@@ -112,19 +100,63 @@ filterItems.forEach((element) => {
 });
 
 // Btn Cart
-
 const detailCart = document.getElementById("detail__cart");
 const btnBackCart = document.querySelector(".back__cart");
 const btnCart = document.querySelector("header button.cart");
+const detailCartRow = document.querySelector("#detail__cart .row");
+const setQty = document.querySelectorAll(".menu figcaption .btn input");
+const addToCart = document.querySelectorAll(".addToCart");
+const qtyInCart = document.querySelector(".qtyInCart");
 
+// function add item to detail cart
+function addItems(params) {
+  const createFigcaption = document.createElement("figcaption");
+  const isiFigcaption = document.createElement("div");
+  const qtyDiv = document.createElement("div");
+  const innerQtyDiv = document.createTextNode(setQty[params].value);
+  const subTotal = document.createElement("div");
+  const innerSubTotal = document.createTextNode("0");
+  isiFigcaption.style.padding = "1em";
+  isiFigcaption.style.background = "black";
+  createFigcaption.appendChild(isiFigcaption);
+  createFigcaption.setAttribute("class", "d-flex");
+  detailCartRow.appendChild(createFigcaption);
+  qtyDiv.appendChild(innerQtyDiv);
+  createFigcaption.appendChild(qtyDiv);
+  subTotal.appendChild(innerSubTotal);
+  createFigcaption.appendChild(subTotal);
+  console.info("oke");
+  // return createFigcaption;
+}
+
+function cartCounter(params) {
+  let count = 0;
+  addToCart[params].addEventListener("click", () => {
+    let result = count + 1;
+    count++;
+    qtyInCart.style.display = "block";
+    qtyInCart.innerHTML = result;
+    addItems(params);
+  });
+}
+
+cartCounter(0);
+cartCounter(1);
+cartCounter(2);
+cartCounter(3);
+cartCounter(4);
+cartCounter(5);
+cartCounter(6);
+cartCounter(7);
+cartCounter(8);
+cartCounter(9);
+
+// Menampilkan Detail Cart
 btnCart.addEventListener("click", () => {
   detailCart.classList.add("show");
-  console.info("oke");
 });
 
-console.info(btnBackCart);
-
+// Menutup Detail Cart
 btnBackCart.addEventListener("click", () => {
   detailCart.classList.remove("show");
-  console.info("oke hapus");
 });
